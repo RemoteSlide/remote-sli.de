65afc9e25fea4c3ea03992d87ebbca90dc15386d
inventivetalent <inventivetalent@inventivegames.de>
2017-05-26 17:20:41 +0200

    remove old files & update paths

--- static/inject/injector.js
+++ static/inject/injector.js
@@ -1,8 +1,8 @@
 (function () {
     $.getScript("https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.1/socket.io.js", function () {
         $("body").append("<div id='remoteSlideOverlayHtmlContainer'></div>");
-        $("#remoteSlideOverlayHtmlContainer").load("https://remote-sli.de/inject/res/overlay.html");
+        $("#remoteSlideOverlayHtmlContainer").load("https://remote-sli.de/inject/controller/overlay.html");
 
-        $.getScript("https://remote-sli.de/inject/res/pageController.js");
+        $.getScript("https://remote-sli.de/inject/controller/pageController.js");
     });
 })();
