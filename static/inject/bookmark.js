3a0be26a5dc26280f6f8180641d8539ee57d4fa9
inventivetalent <inventivetalent@inventivegames.de>
2017-06-10 15:30:37 +0200

    javascript is weird.

--- static/inject/bookmark.js
+++ static/inject/bookmark.js
@@ -12,14 +12,14 @@ javascript:if (!remote_slide) {
             document.getElementsByTagName('head')[0].appendChild(sc);
         };
 
-        if (!$) {
+        if (typeof $ === "undefined") {
             console.info("[RS] Loading JQuery...");
             var sc = document.createElement('SCRIPT');
             sc.type = 'text/javascript';
             sc.src = "https://code.jquery.com/jquery-3.2.1.min.js";
             sc.onload = inject;
             document.getElementsByTagName('head')[0].appendChild(sc);
-        }else{
+        } else {
             console.info("[RS] JQuery already loaded");
             inject();
         }
